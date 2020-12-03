e28e27faed14bad8cc93c12c7199adb05a80259b
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2020-12-03 16:53:40 -0500

    license and line length for java

--- checker.py
+++ checker.py
@@ -1,22 +1,41 @@
 import argparse
 import sys
-import datetime
+from datetime import date
 import re
 import json
+
+CURRENT_YEAR = date.today().year
 
 
 def java_checker(file):
     """
     Checks java file for style and license
     """
-    line = []
+    lines = []
     checks = {
         "license": False,
-        "line_too_long": line,
+        "license_message": "",
+        "line_too_long": lines,
     }
-    # // Copyright (c) 2018, 2009 IBM Corporation and others.
+
     license_re = re.compile(
-        "[/]{2}[ ]*[Cc]opyright[ ]*[(][Cc][)][ ]*([,]{0,1}[ ]*[0-9]{4}){1,2}[ ]*IBM Coporation and others.")
+        "[Cc]opyright[ ]*[(][Cc][)][ ]*([0-9]{4})([,][ ]*([0-9]{4})){0,1}[ ]*")
+    for line_num, line in enumerate(file):
+        if len(line) >= 88:
+            lines.append(line_num + 1)
+        if not checks["license"]:
+            result = license_re.search(line)
+            if result != None:
+                years = result.groups()
+                if years[-1] != CURRENT_YEAR:
+                    checks["license_message"] += "Update the license to the current year.\n"
+                if len(years) > 1 and years[0] >= years[-1]:
+                    checks["license_message"] += "Invalid years in license\n"
+        # print(line_num, line)
+    print(checks["license_message"])
+    if checks["line_too_long"]:
+        print("The following lines are longer than 88 characters:")
+        print(checks["line_too_long"])
     pass
 
 
@@ -29,8 +48,12 @@ def adoc_checker(file):
         "release_date": False,
 
     }
+    license_re = re.compile(
+        "[Cc]opyright[ ]*[(][Cc][)][ ]*([,]{0,1}[ ]*[0-9]{4}){1,2}[ ]*")
     release_date_re = re.compile(
         ":page-releasedate:[ ]*([0-9]{4}[-][0-9]{2}[-][0-9]{2})")
+    for line in file:
+        print(line)
     pass
 
 
@@ -66,13 +89,16 @@ if __name__ == "__main__":
 
     for file in args.infile:
         file_extension = file.name.split('/')[-1].split('.')[-1]
-        if file_extension is 'adoc':
-            adoc_checker(file)
+        print(file_extension)
+        if file_extension == 'adoc':
+            for line_num, line in enumerate(file):
+                print(line_num, line)
+            # adoc_checker(file)
 
-        elif file_extension is 'java':
+        elif file_extension == 'java':
             java_checker(file)
 
-        elif file_extension is 'html':
+        elif file_extension == 'html':
             html_checker(file)
 
         else:
