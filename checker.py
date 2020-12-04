e851f9d8230e2cef0602a6bb19ea82b4f80f9d41
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2020-12-03 19:36:59 -0500

    adoc checker

--- checker.py
+++ checker.py
@@ -1,10 +1,18 @@
 import argparse
 import sys
-from datetime import date
+from datetime import date, datetime
 import re
 import json
 
-CURRENT_YEAR = date.today().year
+today = date.today()
+
+
+def valid(date_text):
+    try:
+        datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        return False
+    return True
 
 
 def java_checker(file):
@@ -13,7 +21,7 @@ def java_checker(file):
     """
     lines = []
     checks = {
-        "license": False,
+        "license": True,
         "license_message": "",
         "line_too_long": lines,
     }
@@ -21,17 +29,17 @@ def java_checker(file):
     license_re = re.compile(
         "[Cc]opyright[ ]*[(][Cc][)][ ]*([0-9]{4})([,][ ]*([0-9]{4})){0,1}[ ]*")
     for line_num, line in enumerate(file):
-        if len(line) >= 88:
+        if len(line) > 88:
             lines.append(line_num + 1)
-        if not checks["license"]:
+        if checks["license"]:
             result = license_re.search(line)
-            if result != None:
+            if result:
                 years = result.groups()
-                if years[-1] != CURRENT_YEAR:
+                if years[-1] != today.year:
                     checks["license_message"] += "Update the license to the current year.\n"
                 if len(years) > 1 and years[0] >= years[-1]:
                     checks["license_message"] += "Invalid years in license\n"
-        # print(line_num, line)
+                checks["license"] = False
     print(checks["license_message"])
     if checks["line_too_long"]:
         print("The following lines are longer than 88 characters:")
@@ -43,17 +51,43 @@ def adoc_checker(file):
     """
     Checks adoc file for style and license
     """
+    lines = []
+    output = ""
     checks = {
-        "license": False,
-        "release_date": False,
-
+        "license": True,
+        "release_date": True,
+        "lines": lines
     }
     license_re = re.compile(
-        "[Cc]opyright[ ]*[(][Cc][)][ ]*([,]{0,1}[ ]*[0-9]{4}){1,2}[ ]*")
+        "[Cc]opyright[ ]*[(][Cc][)][ ]*([0-9]{4})([,][ ]*([0-9]{4})){0,1}[ ]*")
     release_date_re = re.compile(
         ":page-releasedate:[ ]*([0-9]{4}[-][0-9]{2}[-][0-9]{2})")
-    for line in file:
-        print(line)
+
+    for line_num, line in enumerate(file):
+        if len(line) > 120:
+            lines.append(line_num + 1)
+        if checks["license"]:
+            result = license_re.search(line)
+            if result:
+                years = result.groups()
+                if years[-1] != today.year:
+                    output += "Update the license to the current year.\n"
+                if len(years) > 1 and years[0] >= years[-1]:
+                    output += "Invalid years in license\n"
+                checks["license"] = False
+        if checks["release_date"]:
+            result = release_date_re.search(line)
+            if result:
+                date = result.groups()
+                if not valid(date[-1]):
+                    output += f"Release date is invalid: {date[0]} at line {line_num + 1}\n"
+                    output += "The date should be in the form YYYY-MM-DD\n"
+
+    print(output)
+    if checks["lines"]:
+        print("The following lines are longer than 120 characters:")
+        print(checks["lines"])
+        print("Consider wrapping text to improve readability.")
     pass
 
 
@@ -89,11 +123,9 @@ if __name__ == "__main__":
 
     for file in args.infile:
         file_extension = file.name.split('/')[-1].split('.')[-1]
-        print(file_extension)
+
         if file_extension == 'adoc':
-            for line_num, line in enumerate(file):
-                print(line_num, line)
-            # adoc_checker(file)
+            adoc_checker(file)
 
         elif file_extension == 'java':
             java_checker(file)
