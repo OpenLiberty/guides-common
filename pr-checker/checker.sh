36f2e19fb1bdf911ce1cc3a536fb8bce89246cad
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2021-01-26 14:58:58 -0500

    PR checker (#556)
    * converting to python
    
    * denylist and warninglist regex
    
    * regex for copyright license
    
    * license and line length for java
    
    * adoc checker
    
    * update to pr checker
    
    * add line number for errors
    
    * fix
    
    * formatting text edit
    
    * fix copyright year error
    
    * Update checker.py
    
    * html license check
    
    * Update checker.py
    
    * move PR to sub directory
    
    * super-linter starter config
    
    * removed javadoc checker
    
    * Update java.header
    
    * Update java.header
    
    * Update sun_checks.xml
    
    * fixing regex
    
    * removed redundant functions
    
    * using script for workflow
    
    * Update checker.sh
    
    * Update sun_checks.xml
    
    * fixed syntax error
    
    * add action to update license yearly
    
    * fix syntax
    
    * Update license.yml
    
    * Update license.yml
    
    * Update license.yml
    
    * Update license.yml
    
    * Update license.yml
    
    * Update license.yml
    
    * Update license.yml
    
    * add comments to remove on merge
    
    * Update license.yml
    
    * Update checker.py
    
    * Update checker.sh
    
    * Update sun_checks.xml
    
    * Update license.yml
    
    * changed from qa to staging
    
    * Update license.yml
    
    * Update license.yml

---
+++ pr-checker/checker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -o pipefail
+
+repo=$1
+pr_number=$2
+URL="https://api.github.com/repos/$repo/pulls/$pr_number/files"
+FILES=$(curl -s -X GET -G $URL | jq -r '[ .[] |  .filename ]' | tr -d '\n')
+
+if [ $(echo $FILES | jq 'length') = 1 ] && [ $(echo $FILES | jq '.[0]' | tr -d '"') = "README.adoc" ]; then
+    echo "Test can be skipped because only README.adoc was updated"
+    echo "::set-output name=canSkip::true"
+else
+    echo "Need to run test"
+    echo "::set-output name=canSkip::false"
+fi
+
+python3 tools/pr-checker/checker.py --deny tools/pr-checker/deny_list.json --warn tools/pr-checker/warning_list.json $(echo $FILES | jq '.[]' | tr -d '"')
