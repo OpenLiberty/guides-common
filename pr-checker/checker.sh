f74ea7820bee87d9ecd67ec6b9ba99f3db716079
Gilbert Kwan <gkwan@ca.ibm.com>
2023-11-24 10:55:51 -0500

    Update checker.sh

--- pr-checker/checker.sh
+++ pr-checker/checker.sh
@@ -19,6 +19,9 @@ if [ $(echo $ALL_FILES | jq 'length') = 1 ] && [ $(echo $ALL_FILES | jq '.[0]' |
 elif [ $(echo $ALL_FILES | jq 'length') = 1 ] && [ $(echo $ALL_FILES | jq '.[0]' | tr -d '"') = "CONTRIBUTING.md" ]; then
     echo "Test can be skipped because only CONTRIBUTING.md was updated"
     echo "::set-output name=canSkip::true"
+elif [ $(echo $ALL_FILES | jq 'length') = 1 ] && [ $(echo $ALL_FILES | jq '.[0]' | tr -d '"') = "dockerImageTest.sh" ]; then
+    echo "Test can be skipped because only dockerImageTest.sh was updated"
+    echo "::set-output name=canSkip::true"
 else
     echo "Need to run test"
     echo "::set-output name=canSkip::false"
