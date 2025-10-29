563ae5752655b26354100d2ef7cdd93d77e7bbe4
Gilbert Kwan <gkwan@ca.ibm.com>
2025-10-29 10:11:43 -0400

    enable daily build for java 17 and 21 to run draft guides
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- .github/workflows/get-guides-java21.py
+++ .github/workflows/get-guides-java21.py
@@ -1,6 +1,10 @@
 import json
 
 JSON_PATH = ".github/workflows/guides-java21.json"
+DRAFT_JSON_PATH = ".github/workflows/drafts-to-test-java21.json"
 
 if __name__ == "__main__":
-    print(json.load(open(JSON_PATH)))
+    output = json.load(open(JSON_PATH))
+    draft = json.load(open(DRAFT_JSON_PATH))
+    output += draft
+    print(output)
